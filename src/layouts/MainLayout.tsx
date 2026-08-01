1994af4d911b4923a6f9fafb1cbf3e50842254f9
Aditi Jha <aditiashokjha03@gmail.com>
2026-08-01 16:05:39 +0530

    industries updated

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Navbar } from '../components/Navbar/Navbar';
 import { Footer } from '../components/Footer/Footer';
 
 export const MainLayout: React.FC = () => {
+  const location = useLocation();
+  const isIndustriesPage = location.pathname === '/industries' || location.pathname === '/industries/';
+
   return (
     <div className="flex flex-col min-h-screen">
       {/* Premium Floating Glass Navbar */}
@@ -15,7 +18,7 @@ export const MainLayout: React.FC = () => {
       </div>
 
       {/* Footer */}
-      <Footer />
+      {!isIndustriesPage && <Footer />}
     </div>
   );
 };
